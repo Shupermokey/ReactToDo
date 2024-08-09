4ea2f1ff749da772c919bd3ace7ad914ade498e7
Shupermokey <a.e.sobieraj@gmail.com>
2024-08-08 20:44:49 -0500

    batman

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ProjectsSidebar() {
+  return (
+    <aside>
+      <h2>Your Projects</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+    </aside>
+  );
+}
